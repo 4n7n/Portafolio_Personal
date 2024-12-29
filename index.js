cea19ed29c75fea6ccfb1d7de59090d300be4101
4n7n <anthonybonillaparedes7@gmail.com>
2024-12-30 00:12:50 +0100

    archivo removido

--- index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll('.fade-in');
-    sections.forEach((section) => {
-        section.classList.add('visible');
-    });
-});
